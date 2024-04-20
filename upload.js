08c5d6e36daef15f7631d88d57d3acedcddefa39
ikigai0822 <pranaymayal17@gmail.com>
2024-04-21 00:52:04 +0530

    updated uplod file

--- upload.js
+++ upload.js
@@ -23,12 +23,9 @@ form.addEventListener('submit', (e) => {
  e.preventDefault(); // Correctly calling the preventDefault method
 
  const formData = new FormData(form);
- console.log(typeof formData);
- console.log(formData.getAll);
- console.log(formData);
  
- for (let pair of formData.entries()) {
-  console.log(pair[0]+ ', '+ pair[1]); 
+for (item of formData){
+  console.log(item[0],item[1]);
 }
 
 fetch('http://65.0.110.20:8007/torrents', {
